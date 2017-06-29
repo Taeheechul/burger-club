e76b8dfa49f23b82fb9805e8af209375c8244289
Jewel Clarke-Henry <YukiTapionCrow@gmail.com>
2017-06-29 18:01:58 +0000

    Update

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
+/*global $*/
 $(document).ready(function() {
-
-   
+    $('li').mouseenter(function(){
+        $('li').fadeOut(slow, 0.5);
+    }).mouseleave(function(){
+        $('li').fadeIn(slow, 1);
+    });
     
-
+    
+    $('.btn').click(function(){
+        alert(“HI - <name> "thank you for signing up! We will send an email to" 
+        <email> "shortly"
+        });
 });
